6428945eadb0576dbbd5c9355c3be56b921b5414
Craig Tweedy <craig.tweedy@hedgehoglab.com>
2015-11-12 15:25:00 +0000

    Bumped version to 0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'django-rest-versioning',
   packages = ['django-rest-versioning'],
-  version = '0.1',
+  version = '0.2',
   description = 'A lightweight mixin to allow routing Django Rest Framework endpoints to different classes depending on version.',
   author = 'Craig Tweedy',
   author_email = 'ahoy@craigtweedy.co.uk',
   url = 'https://github.com/craigtweedy/django-rest-versioning',
-  download_url = 'https://github.com/craigtweedy/django-rest-versioning/tarball/0.1',
+  download_url = 'https://github.com/craigtweedy/django-rest-versioning/tarball/0.2',
   keywords = ['django', 'django-rest-framework', 'version', 'versioning', 'api'],
   classifiers = [],
 )
