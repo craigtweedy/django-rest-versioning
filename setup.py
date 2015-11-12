f0d910a1d634b8abfc5a41e8593142bf2af70145
Craig Tweedy <craig.tweedy@hedgehoglab.com>
2015-11-12 15:30:03 +0000

    Bumping version to fix bad PyPi upload

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'django-rest-versioning',
   packages = ['django-rest-versioning'],
-  version = '0.2',
+  version = '0.2.0',
   description = 'A lightweight mixin to allow routing Django Rest Framework endpoints to different classes depending on version.',
   author = 'Craig Tweedy',
   author_email = 'ahoy@craigtweedy.co.uk',
   url = 'https://github.com/craigtweedy/django-rest-versioning',
-  download_url = 'https://github.com/craigtweedy/django-rest-versioning/tarball/0.2',
+  download_url = 'https://github.com/craigtweedy/django-rest-versioning/tarball/0.2.0',
   keywords = ['django', 'django-rest-framework', 'version', 'versioning', 'api'],
   classifiers = [],
 )
